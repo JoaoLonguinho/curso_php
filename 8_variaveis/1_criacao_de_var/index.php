2d7fa5dbd111e2d82adc5a5f9732aceb364ec22f
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-01-30 15:33:44 -0300

    Inicio novo modulo

---
+++ 8_variaveis/1_criacao_de_var/index.php
@@ -0,0 +1,16 @@
+<?php
+
+ $teste = "algum texto";
+
+ echo $teste;
+
+ $num = 10;
+
+ echo "<br>";
+ echo $num;
+
+ $_nome = "João";
+ echo "<br>";
+ echo $_nome;
+ $velocidadeMaxima = 100;
+ echo "<br> $velocidadeMaxima";
---
+++ 8_variaveis/2_exercicio_8/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$_name = "Lucas";
+$_age = 32;
+$money = 104.23;
+
+echo "$_name, $_age anos, com $money reais na conta bancária";
