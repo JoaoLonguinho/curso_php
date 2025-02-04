8e892a75a23f4bcb95e6a383e31e5008e413fe97
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-04 09:18:25 -0300

    Aula $ variavel da variavel $$

---
+++ 8_variaveis/3_exercicio_9/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$num1 = 9;
+$num2 = 5.6;
+$sum = $num1 + $num2;
+
+echo $sum;
---
+++ 8_variaveis/4_var_de_var/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$x = 'idade';
+
+echo "$x é uma variavel <br/>";
+
+$$x = 14;
+
+echo "João tem $idade anos de $x";
