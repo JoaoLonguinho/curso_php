3e6b7af543ece4239955d9db11de68ab75d3fbcc
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-04 14:29:29 -0300

    Exercicios operadores matematicos

--- 9_operacoes_e_expressoes/4_exercicio_11/index.php
+++ 9_operacoes_e_expressoes/4_exercicio_11/index.php
@@ -6,3 +6,9 @@ $c = $a * $b;
 echo $c . "<br/>";
 
 echo gettype($c);
+echo "<br/>";
+echo gettype([]);
+echo "<br/>";
+echo gettype('string');
+echo "<br/>";
+echo gettype(12.12);
---
+++ 9_operacoes_e_expressoes/5_exercicio_12/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 12;
+$b = 23;
+
+echo $c = $a + $b;
+echo "<br/>";
+echo $d = $c - $b;
+echo "<br/>";
+echo $e = $d / $c;
+echo "<br/>";
+echo $f = $e * $d;
+echo "<br/>";
