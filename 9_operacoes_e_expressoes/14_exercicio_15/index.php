63f6c40abf24a40cad4016f3f4291165ac544bad
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-05 11:30:42 -0300

    Operador identico / exercicio

---
+++ 9_operacoes_e_expressoes/14_exercicio_15/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = 'João';
+$outroNome = 'João';
+$outroDoOutroNome = 'Julio';
+ if ($nome == $outroNome){
+    echo "comparativo de nomes funcionou";
+ }
+ 
+ if ($outroNome == $outroDoOutroNome){
+    echo "comparativo de outros nomes funcionou";
+ }
---
+++ 9_operacoes_e_expressoes/15_identico_a/index.php
@@ -0,0 +1,13 @@
+<?php
+
+if("João" === "João"){
+    echo 'Funcionou ';
+}
+
+if("João" === "joão"){
+    echo 'Funcionou ';
+}
+
+if(5 === "5"){
+    echo "funcionou";
+}
