7a911fdb116865fa44f9f3100c666e5a81e94b8e
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-05 11:46:22 -0300

    Menor ou igual

---
+++ 9_operacoes_e_expressoes/20_menor_ou_igual/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 2;
+$b = 3;
+$c = 4;
+$d = 4;
+
+if($a < $b){
+    echo "funcionou 1" . "<br/>";
+}
+if($b <= $a){
+    echo "funcionou 2" . "<br/>";
+}
+if($c <= $d){
+    echo "funcionou 3" . "<br/>";
+}
