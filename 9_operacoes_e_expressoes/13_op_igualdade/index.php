64facd3044d997939da99da241a537d5509228cd
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-05 11:22:06 -0300

    Comparação

---
+++ 9_operacoes_e_expressoes/13_op_igualdade/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$a = 2;
+$b = 8;
+
+if($a == $b){
+    echo 'as duas váriaveis tem o mesmo valor';
+}
+
+$i = 4;
+$j = 4;
+
+if($i == $j){
+    echo 'as duas váriaveis tem o mesmo valor 2';
+}
