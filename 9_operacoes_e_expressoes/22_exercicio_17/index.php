44aee96306f872b20cd7ac6b1fec01451edb5097
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-05 14:32:56 -0300

    OR

---
+++ 9_operacoes_e_expressoes/22_exercicio_17/index.php
@@ -0,0 +1,13 @@
+<?php
+
+if (15 > 5 && "João" === "João"){
+    echo "funcionou 1 <br/>";
+}
+
+if ("teste" > 5 && 1){
+    echo "funcionou 2<br/>";
+}
+
+if (2 == 3 && 5 >= 3 ){
+    echo "funcionou 3<br/>";
+}
---
+++ 9_operacoes_e_expressoes/23_OR/index.php
@@ -0,0 +1,12 @@
+<?php
+
+if(1> 2 || 87>3 ){
+    echo "funcionou 1<br/>";
+}
+
+if(5> 2 || 1>3 ){
+    echo "funcionou 2<br/>";
+}
+if(1> 2 || 1>3 || 4>3){
+    echo "funcionou 3<br/>";
+}
