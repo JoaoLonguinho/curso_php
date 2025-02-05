e49a17e70199637f0596c765022613b38790602a
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-05 11:39:16 -0300

    Exercicio comparacoes igualdade, diferenca , identico e nao identico

---
+++ 9_operacoes_e_expressoes/18_exercicio_16/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 5;
+$b = 3;
+
+if($a == $b){
+    echo "funcionou 1" . "<br/>";
+}
+if($a === $b){
+    echo "funcionou 2" . "<br/>";
+}
+if($a != $b){
+    echo "funcionou 3" . "<br/>";
+}
+if($a !== $b){
+    echo "funcionou 4" . "<br/>";
+}
+
