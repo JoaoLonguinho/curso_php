9868ced3dbce9f6675732da61fa3a5f4cb5cfae4
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-05 15:04:48 -0300

    Exercicio final do módulo

---
+++ 9_operacoes_e_expressoes/29_operador_ternario/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$a = 20;
+$b = 5;
+
+echo $a > $b ? "sim, A é maior que B" : "não, A não é maior que B";
+
+echo $a > $b ? "sim, A é maior que B" : "não, A não é maior que B";
---
+++ 9_operacoes_e_expressoes/30_exercicio_20/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$numO = (int) "13";
+$numT = 27;
+
+echo $numO <= $numT ? "Primeira validação está correta" : "Segunda validação, correta";
+echo "<br/>";
+echo $numO >= $numT ? "Primeira validação está correta" : "Segunda validação, correta";
