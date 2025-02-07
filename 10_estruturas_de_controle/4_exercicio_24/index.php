0535ffc71b2f9121cb55bb5678f4c9eaef632bb4
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-07 12:49:16 -0300

    Exercicio checagem de tipo utilizando if else

---
+++ 10_estruturas_de_controle/4_exercicio_24/index.php
@@ -0,0 +1,38 @@
+<?php
+
+$nome = "João";
+$idade = 27;
+$saldo = 10.50;
+$hobbes = [
+    "jogar",
+    "sair",
+    "viajar"
+];
+
+$msg = "É um inteiro";
+$msgNegative = "não é um inteiro";
+
+if(gettype($nome) == 'integer'){
+    echo $msg . "<br/>";
+}
+else {
+    echo $msgNegative . "<br/>";
+}
+if(gettype($idade) == 'integer'){
+    echo $msg . "<br/>";
+}
+else {
+    echo $msgNegative . "<br/>";
+}
+if(gettype($saldo) == 'integer'){
+    echo $msg . "<br/>";
+}
+else {
+    echo $msgNegative . "<br/>";
+}
+if(gettype($hobbes) == 'integer'){
+    echo $msg . "<br/>";
+}
+else {
+    echo $msgNegative . "<br/>";
+}
