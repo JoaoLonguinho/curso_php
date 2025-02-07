fa2776058c5f49a717572dbc40cf0b249c6e48bb
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-07 13:53:40 -0300

    Tornando o código mais inteligente utilizando função (por fora da aula)

--- 10_estruturas_de_controle/6_exercicio_26/index.php
+++ 10_estruturas_de_controle/6_exercicio_26/index.php
@@ -5,51 +5,23 @@ $b = 3;
 $c = "João";
 $e =  "Longuinho";
 
-if(is_int($a)){
-    $d = $a * 2;
-    if($d > 100){
-        echo "O número é maior que 100<br/>";       
+
+function verificaNumero($num){
+    if(is_int($num)){
+        $d = $num * 2;
+        if($d > 100){
+            echo "O número é maior que 100<br/>";       
+        }
+        else {
+            echo "O número é menor que 100<br/>";
+       }
     }
     else {
-        echo "O número é menor que 100<br/>";
-   }
-}
-else {
-    echo "Não é um inteiro <br/>";
-}
-if(is_int($b)){
-    $d = $b * 2;
-    if($d > 100){
-        echo "O número é maior que 100<br/>";       
+        echo "Não é um inteiro <br/>";
     }
-    else {
-        echo "O número é menor que 100<br/>";
-   }
 }
-else {
-    echo "Não é um inteiro <br/>";
-}
-if(is_int($c)){
-    $d = $c * 2;
-    if($d > 100){
-        echo "O número é maior que 100<br/>";       
-    }
-    else {
-        echo "O número é menor que 100<br/>";
-   }
-}
-else {
-    echo "Não é um inteiro <br/>";
-}
-if(is_int($e)){
-    $d = $e * 2;
-    if($d > 100){
-        echo "O número é maior que 100<br/>";       
-    }
-    else {
-        echo "O número é menor que 100<br/>";
-   }
-}
-else {
-    echo "Não é um inteiro <br/>";
-}
+
+verificaNumero($a);
+verificaNumero($b);
+verificaNumero($c);
+verificaNumero($e);
