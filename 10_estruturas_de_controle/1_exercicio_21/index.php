2ac37a8104fc26d380cd4338b1bd3128cd783747
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-07 12:33:59 -0300

    Exercicios if

---
+++ 10_estruturas_de_controle/1_exercicio_21/index.php
@@ -0,0 +1,13 @@
+<?php
+
+if( 5 > 2){
+    echo "sim, 5 é maior que dois";
+}
+
+if("Matheus" != "Pedro"){
+    echo "Sim, Matheus é diferente de pedro";
+}
+
+if( 12 >=  11){
+    echo "Sim, 12 é maior ou igual a 11";
+}
---
+++ 10_estruturas_de_controle/2_exercicio_22/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 12;
+$b = 20;
+$c = 40;
+
+if($a >= 18){
+
+    echo "ele tem $a anos, então é maior de idade <br/>?";
+}
+if($b >= 18){
+
+    echo "ele tem $b anos, então é maior de idade <br/>";
+}
+if($c >= 18){
+
+    echo "ele tem $c anos, então é maior de idade <br/>";
+}
