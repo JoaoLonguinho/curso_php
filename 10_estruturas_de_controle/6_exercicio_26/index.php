cdf3c5f37715b2da146f74b07a6c3c0cbc2ec414
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-07 13:49:48 -0300

    Exercicio if aninhado

---
+++ 10_estruturas_de_controle/6_exercicio_26/index.php
@@ -0,0 +1,55 @@
+<?php
+
+$a = 1;
+$b = 3;
+$c = "João";
+$e =  "Longuinho";
+
+if(is_int($a)){
+    $d = $a * 2;
+    if($d > 100){
+        echo "O número é maior que 100<br/>";       
+    }
+    else {
+        echo "O número é menor que 100<br/>";
+   }
+}
+else {
+    echo "Não é um inteiro <br/>";
+}
+if(is_int($b)){
+    $d = $b * 2;
+    if($d > 100){
+        echo "O número é maior que 100<br/>";       
+    }
+    else {
+        echo "O número é menor que 100<br/>";
+   }
+}
+else {
+    echo "Não é um inteiro <br/>";
+}
+if(is_int($c)){
+    $d = $c * 2;
+    if($d > 100){
+        echo "O número é maior que 100<br/>";       
+    }
+    else {
+        echo "O número é menor que 100<br/>";
+   }
+}
+else {
+    echo "Não é um inteiro <br/>";
+}
+if(is_int($e)){
+    $d = $e * 2;
+    if($d > 100){
+        echo "O número é maior que 100<br/>";       
+    }
+    else {
+        echo "O número é menor que 100<br/>";
+   }
+}
+else {
+    echo "Não é um inteiro <br/>";
+}
