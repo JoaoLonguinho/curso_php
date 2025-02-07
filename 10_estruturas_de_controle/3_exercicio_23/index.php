72043a2b599741005d74b7d4a8f8d6770fe98ea9
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-07 12:40:28 -0300

    Else

---
+++ 10_estruturas_de_controle/3_exercicio_23/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 12;
+$b = 20;
+$c = 40;
+
+$msg = "você é menor de idade";
+if($a >= 18){
+
+    echo "ele tem $a anos, então é maior de idade <br/>?";
+} else {
+    echo $msg;
+}
+if($b >= 18){
+
+    echo "ele tem $b anos, então é maior de idade <br/>";
+} else {
+    echo $msg;
+}
+if($c >= 18){
+
+    echo "ele tem $c anos, então é maior de idade <br/>";
+} else {
+    echo $msg;
+}
