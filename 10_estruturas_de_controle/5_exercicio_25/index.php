1e380fe8694e15e2b821cedae0e68589135c3f18
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-07 13:33:20 -0300

    Exercicio peso

---
+++ 10_estruturas_de_controle/5_exercicio_25/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$peso = 67;
+
+$pesoLimite = 80;
+
+if($peso < $pesoLimite){
+    echo "Sua bagagem é de $peso KGs, está dentro dos limites";
+}
+else {
+    echo "Sua bagagem é de $peso KGs e ultrapassa o limite";
+}
