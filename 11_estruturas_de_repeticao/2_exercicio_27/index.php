47bbd602ec9700add064d72157f2ef1d1aade4f4
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-10 11:24:59 -0300

    Exercicio while acessando valores que são apenas strings

---
+++ 11_estruturas_de_repeticao/2_exercicio_27/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$itens = ['João', 27, "Dev", 10.00, "Bianca", 24, "Chandelier", 290.000, "Unhas", 30];
+$counter = 0;
+
+while ($counter < sizeof($itens)){
+
+    if(is_string($itens[$counter])){
+        echo $itens[$counter];
+    }
+
+    $counter++;
+}
