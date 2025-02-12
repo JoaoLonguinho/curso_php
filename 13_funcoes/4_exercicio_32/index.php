26264a82d51007571138cae716aee5027fc9810f
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-12 10:31:15 -0300

    Exercicio

---
+++ 13_funcoes/4_exercicio_32/index.php
@@ -0,0 +1,12 @@
+<?php
+
+function multiplicacao(){
+    $a = 3;
+    $b = 5.5;
+    $c = 12;
+
+    $resultado = $a * $b * $c;
+    echo "A Multiplicação dos números resulta em:" . $resultado;
+}
+
+multiplicacao();
