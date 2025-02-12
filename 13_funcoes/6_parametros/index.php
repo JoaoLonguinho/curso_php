9acca3dac1bb1afa7d32bc5c2ab737b325f30fd0
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-12 10:40:21 -0300

    Definindo um valor padrao caso o parametro nao seja passado

--- 13_funcoes/6_parametros/index.php
+++ 13_funcoes/6_parametros/index.php
@@ -1,10 +1,11 @@
 <?php
 
-function velocidadeMaxima($vel) {
-    echo "O carro atinge a velocidade máxima de: $vel km/h";
+function velocidadeMaxima($vel = 100) {
+    echo "O carro atinge a velocidade máxima de: $vel km/h <br/>";
 }
 
 velocidadeMaxima(200);
 velocidadeMaxima(300);
 velocidadeMaxima(400);
 velocidadeMaxima(500);
+velocidadeMaxima();
