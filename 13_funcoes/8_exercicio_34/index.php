973a2ae8b71224e052d635fc11633d9a36a3dcb5
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-12 12:51:00 -0300

    Exercicio checar se e par ou impar

---
+++ 13_funcoes/8_exercicio_34/index.php
@@ -0,0 +1,15 @@
+<?php
+
+function checkEvenOdd($number){
+    if($number % 2 == 0){
+        echo "O número $number é Par <br/>";
+    }
+    else{
+        echo "O número $number é Impar <br/>";
+    }
+}
+for($a = 0; $a <= 10; $a++){
+    checkEvenOdd($a);
+}
+
+
