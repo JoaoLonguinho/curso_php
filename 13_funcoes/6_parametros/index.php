e890e759ee7d92266b1794a75adfe58fd2aebd0e
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-12 10:44:29 -0300

    Adicionando validacao caso o parametro seja uma string

--- 13_funcoes/6_parametros/index.php
+++ 13_funcoes/6_parametros/index.php
@@ -1,9 +1,14 @@
 <?php
 
-$uno = 200
+$uno = 200;
 
 function velocidadeMaxima($vel = 100) {
-    echo "O carro atinge a velocidade máxima de: $vel km/h <br/>";
+    if(!is_int($vel)){
+        echo "O valor $vel, não é um valor válido, por favor insira um valor inteiro";
+    }
+    else{
+        echo "O carro atinge a velocidade máxima de: $vel km/h <br/>";
+    };
 }
 
 velocidadeMaxima(200);
@@ -11,3 +16,4 @@ velocidadeMaxima(300);
 velocidadeMaxima(400);
 velocidadeMaxima(500);
 velocidadeMaxima($uno);
+velocidadeMaxima("Teste");
