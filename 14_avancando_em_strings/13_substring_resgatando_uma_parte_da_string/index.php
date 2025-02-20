a2a647b068735850b45fccb9a0d77056a3b5f94e
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-20 12:11:49 -0300

    Subtring

--- 14_avancando_em_strings/13_substring_resgatando_uma_parte_da_string/index.php
+++ 14_avancando_em_strings/13_substring_resgatando_uma_parte_da_string/index.php
@@ -8,4 +8,14 @@ $minha = substr($str, 9, 5);
 
 echo $minha . "<br/>";
 
-echo $str; # string original não é modificada.
+echo $str . "<br/>"; # string original não é modificada.
+
+$str2 = "Isso é um novo teste";
+
+$str2Separada = substr($str2, 11);
+
+echo $str2Separada . "<br/>";
+
+$str2SeparadaComNegativo = substr($str2, 11, -5);
+
+echo $str2SeparadaComNegativo . "<br/>";
