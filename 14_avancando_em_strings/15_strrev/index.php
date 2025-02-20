c3f7f16b7e8f02e7f6dad2cc15791fd5a9e75cae
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-20 12:24:50 -0300

    str_repeat

---
+++ 14_avancando_em_strings/15_strrev/index.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$str = "teste de string no reverso";
+
+$strReversa = strrev($str);
+
+echo $strReversa;
---
+++ 14_avancando_em_strings/16_str_repeat/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$str = "tem dois cachorros, o Pete e o Repete, o Pete correu, quem sobrou? Repete! <br/>";
+
+$strRepeat = str_repeat($str, 1000);
+
+echo $strRepeat;
