203fb09545a92f0004db47e994d2efd8c14efcf2
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-21 11:28:45 -0300

    Array multidimensional + count

---
+++ 15_arrays/5_count_numero_de_elementos/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr = [1,2,3];
+
+echo count($arr);
+echo "<br>";
+
+$arr2 = range(1, 20);
+
+echo count($arr2);
+echo "<br>";
+
+$arrAssoc = ["Nome: " => "João", "Idade: " => 27, "Lazer" => "Jogar, sair e programar"];
+
+echo count($arrAssoc);
