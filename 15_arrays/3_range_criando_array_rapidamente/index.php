acfe1269598fb265bbc36c88ff3d8ecdee500910
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-21 11:13:29 -0300

    range

---
+++ 15_arrays/3_range_criando_array_rapidamente/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$arr = range(1,20);
+
+print_r($arr);
+echo "<br/>";
+
+$arr2 = range(13, 199);
+
+print_r($arr);
+echo "<br/>";
+
+$arr3 = range(0, 150, 10); #Inicio do array, quantidade final do array, e de quantos em quantos números ele vai pular.
+
+print_r($arr3);
+echo "<br/>";
