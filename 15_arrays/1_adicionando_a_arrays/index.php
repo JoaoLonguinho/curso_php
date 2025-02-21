6a9ad41db5e93ba0145318c0a22e0d8fee432742
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-21 11:03:58 -0300

    Novo módulo, adicionando valores em cada posição do array + array associativo

---
+++ 15_arrays/1_adicionando_a_arrays/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$arr = [];
+
+print_r($arr);
+
+$arr[0] = "João";
+echo "<br/>";
+print_r($arr);
+
+
+$arr[6] = 28;
+echo "<br/>";
+print_r($arr);
+
+$arr[6] += 809;
+
+echo "<br/>";
+print_r($arr);
+
+$arrAssoc = [];
+
+$arrAssoc["carro"] = "vermelho"; #inserindo dados em um array associativo.
+
+echo "<br/>";
+print_r($arrAssoc);
