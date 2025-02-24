77000a0b7ae897e7f1033ca05f0e26164b9ed58d
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-24 10:26:49 -0300

    Exercicio 49

---
+++ 15_arrays/18_exercicio_49/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = "Thor";
+$tamanho = "Pequeno";
+$idade = 0.11;
+$humor = "Travesso";
+
+$arr = compact("nome", "tamanho", "idade", "humor");
+
+foreach($arr as $caracteristica){
+    echo $caracteristica . "<br/>";
+}
