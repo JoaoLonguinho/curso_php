2c72db2d342c0fce2abcb3675a2f889b46bda5c3
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-25 10:31:02 -0300

    array_merge

---
+++ 15_arrays/28_array_sum/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$arr = [
+    2,3,4,1,3,24
+];
+
+$result = array_sum($arr);
+
+echo "O resultado da soma do array é: $result <br/>";
---
+++ 15_arrays/29_unindo_arrays/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$arr1 = [2,4,6];
+$arr2 = [5,1,3];
+
+$together = array_merge($arr1, $arr2);
+
+print_r($together);
+echo "<br/>";
+print_r($arr1);
