da7de114005126af6df10fad2bd778b99fa24ee4
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-25 09:49:08 -0300

    sort rsort

---
+++ 15_arrays/23_sort/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$numbers = [
+    12,32,0,23,53
+];
+
+sort($numbers);
+
+print_r($numbers);
+echo "<br/>";
+
+rsort($numbers);
+
+print_r($numbers);
+echo "<br/>";
+
+$nomes = [
+    "João",
+    "Bianca",
+    "Carlos",
+    "Rael"
+];
+
+sort($nomes);
+
+print_r($nomes);
