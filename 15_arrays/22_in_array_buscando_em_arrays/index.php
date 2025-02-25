0ec33ec442eba10cb6b1d536d356afad521a44df
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-25 09:45:08 -0300

    in_array

---
+++ 15_arrays/22_in_array_buscando_em_arrays/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$nomes = [
+    "João",
+    "Bianca",
+    "Carlos",
+    "Rael"
+];
+
+if(in_array("Carlos", $nomes)){
+    echo "O item está no array";
+}
+else{
+    echo "O item não está no array";
+}
