db73dd3e18d969bb11fa2b8f3e24250c61a79416
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-02-27 14:29:46 -0300

    Adicionando a materia

--- 16_OOP/13_exercicio_56/index.php
+++ 16_OOP/13_exercicio_56/index.php
@@ -3,21 +3,21 @@
 class Humano {
     public $nome;
     public $idade;
-    protected function falar(){
-        echo $this->nome . " tem " . $this->idade . " anos de idade";
+    protected function falar($materia){
+        echo $this->nome . " tem " . $this->idade . " anos de idade e ensino " . $materia;
     }
 }
 
 class Professor extends Humano {
     public $materia;
-    public function getFalar(){
-        return $this->falar();
+    public function getFalar($materia){
+        return $this->falar($materia);
     }
 }
 
 $Giovanni = new Professor;
-
 $Giovanni->nome = "Giovanni";
 $Giovanni->idade = 32;
+$Giovanni->materia = "Geografia";
 
-$Giovanni->getFalar();
+$Giovanni->getFalar($Giovanni->materia);
