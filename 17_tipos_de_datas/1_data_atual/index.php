b60ffb51ab576acaef9ad0f21e2d9f7151d1a929
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-03-05 12:14:17 -0300

    Novo modulo - Date

---
+++ 17_tipos_de_datas/1_data_atual/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$dataHoje = date('d/m/y');
+
+echo $dataHoje . "<br/>";
+
+$dataHojeCompletaTexto = date('D /M - Y');
+
+echo $dataHojeCompletaTexto . "<br/>";
+
+$dataFormatoFamoso = date('d/m/Y');
+
+echo $dataFormatoFamoso . "<br/>";
