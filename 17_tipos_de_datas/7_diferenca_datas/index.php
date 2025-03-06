bf43ec82d10dbfe54dd55694a57a6df73bedfb49
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-03-06 12:08:17 -0300

    diff

---
+++ 17_tipos_de_datas/7_diferenca_datas/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$dataA = new DateTime();
+$dataB = new DateTime();
+
+$dataB->setDate(2004, 12, 23);
+
+print_r($dataA);
+
+echo "<br>";
+
+print_r($dataB);
+
+
+$diff = $dataA->diff($dataB);
+
+echo "<br>";
+
+print_r($diff);
