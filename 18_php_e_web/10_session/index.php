204876e0609a49c70d1d9edff79c26717009b3f4
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-03-11 11:18:12 -0300

    Session

---
+++ 18_php_e_web/10_session/index.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    //print_r($_SESSION);
+
+    $_SESSION["nome"] = "João";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá Session</h1>
+</body>
+</html>
