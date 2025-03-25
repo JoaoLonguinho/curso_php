a7db1aeaaf27a29160f5af7d836c45458f2b39ad
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-03-25 16:47:41 -0300

    Criando select de items

--- 22_design_patterns/1_DAO/dao/carDAOMySql.php
+++ 22_design_patterns/1_DAO/dao/carDAOMySql.php
@@ -12,7 +12,27 @@ class CarDAO implements CarDAOInterface{ // Está implementando a interface da c
 
     public function findAll(){
 
+        $cars = [];
+
+        $stmt = $this->conn->query("SELECT * FROM cars");
+
+        $data = $stmt->fetchAll();
+
+        foreach($data as $item){
+
+            $car = new Car();
+
+            $car->setId($item["id"]);
+            $car->setBrand($item["brand"]);
+            $car->setKm($item["km"]);
+            $car->setColor($item["color"]);
+
+            $cars[] = $car;
+        }
+
+        return $cars;
     }
+    
 
     public function create(Car $car){
         $stmt = $this->conn->prepare("INSERT INTO cars (brand, km, color) VALUES (:brand, :km, :color)");
--- 22_design_patterns/1_DAO/index.php
+++ 22_design_patterns/1_DAO/index.php
@@ -1,6 +1,10 @@
 <?php
     include_once("db.php");
+    include_once("dao/carDAOMySql.php");
 
+    $carDao = new CarDao($conn);
+
+    $cars = $carDao->findAll();
 ?>
 
 <h1>Insira um carro:</h1>
@@ -19,3 +23,8 @@
     </div>
     <input type="submit" value="Enviar">
 </form>
+<ul>
+    <?php foreach($cars as $car): ?>
+        <li><?= $car->getBrand(); ?> - <?= $car->getKm(); ?> - <?= $car->getColor(); ?></li>
+    <?php endforeach; ?>
+</ul>
