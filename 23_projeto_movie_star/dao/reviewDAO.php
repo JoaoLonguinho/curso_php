45e99a9e9342b60d447d49e0855a0affd3073964
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-04-19 22:35:30 -0300

    Adicionando metodos para inserir a critica no filme

--- 23_projeto_movie_star/dao/reviewDAO.php
+++ 23_projeto_movie_star/dao/reviewDAO.php
@@ -12,7 +12,7 @@ class ReviewDAO implements ReviewDAOInterface
 
     private $message;
 
-    public function __construct($conn, $url, $message)
+    public function __construct(PDO $conn, $url)
     {
         $this->conn = $conn;
         $this->url = $url;
@@ -31,7 +31,24 @@ class ReviewDAO implements ReviewDAOInterface
         return $reviewObject;
     }
     public function create(Review $review){
+        $rating = $review->rating;
+        $review = $review->review;
+        $movies_id = $review->movies_id;
+        $users_id = $review->users_id;
 
+        $stmt = $this->conn->prepare("INSERT INTO movies (rating, review, movies_id, users_id) 
+        VALUES 
+        (:rating, :review, :movies_id, :users_id)");
+
+        $stmt->bindParam("rating", $rating);
+        $stmt->bindParam("review", $review);
+        $stmt->bindParam("movies_id", $movies_id);
+        $stmt->bindParam("users_id", $users_id);
+
+        $stmt->execute();
+
+        //Mensagem de sucesso ao adicionar o filme
+        $this->message->setMessage("Comentário e notas publicados!", "success", "index.php");
     }
     public function getMoviesReview($id){
 
--- 23_projeto_movie_star/review_process.php
+++ 23_projeto_movie_star/review_process.php
@@ -2,19 +2,52 @@
 require_once "globals.php";
 require_once "db.php";
 require_once "models/Movie.php";
+require_once "models/Review.php";
 require_once "models/Message.php";
 require_once "dao/userDAO.php";
 require_once "dao/movieDAO.php";
+require_once "dao/reviewDAO.php";
 
 #Recebendo tipo form
 $type = filter_input(INPUT_POST, "type");
 
+
 $message = new Message($BASE_URL);
 $movieDao = new MovieDao($conn, $BASE_URL);
 $userDao = new UserDao($conn, $BASE_URL);
+$reviewDao = new ReviewDao($conn, $BASE_URL);
 
 $userData = $userDao->verifyToken();
 
 if($type === "create"){
+    //Recebendo dados do post
+    $rating = filter_input(INPUT_POST, "rating");
+    $review = filter_input(INPUT_POST, "review");
+    $movies_id = filter_input(INPUT_POST, "movies_id");
+    $users_id = $userData->id;
 
+    $reviewObject = new Review();
+
+    $movieData = $movieDao->findById($movies_id);
+
+    if($movieData){
+        if(!empty($rating) && !empty($review) && !empty($movies_id)){
+            $reviewObject->rating = $rating;
+            $reviewObject->review = $review;
+            $reviewObject->movies_id = $movies_id;
+            $reviewObject->users_id = $users_id;
+
+            $reviewDao->create($reviewObject);
+        }
+        else{
+            $message->setMessage("Nova e comentário não podem estar vazias.", "error", "index.php");    
+        }
+
+    }
+    else{
+        $message->setMessage("Filme não encontrado", "error", "index.php");    
+    }
+
+} else {
+    $message->setMessage("Informações inválidas", "error", "index.php");
 }
